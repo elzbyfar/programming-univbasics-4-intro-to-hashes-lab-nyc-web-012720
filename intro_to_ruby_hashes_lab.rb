e77ec0066969a8b9ba4ac8e5c05b39b96e77734b
Luis Alejo <lou.m.alejo@gmail.com>
2020-01-10 00:27:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,11 +24,14 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  created_hash = {
+    key => value, 
+  }
+  return created_hash
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  
 end
 
 def update_counting_hash(hash, key)

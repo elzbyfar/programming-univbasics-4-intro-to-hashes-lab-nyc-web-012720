f3da37ffae080a86bd444e29b8d5093831a9feab
Luis Alejo <lou.m.alejo@gmail.com>
2020-01-10 00:35:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,8 +37,8 @@ end
 def update_counting_hash(hash, key)
   
   if hash[key]
-    return hash[key] = hash[key] + 1
+    hash[key] = hash[key] + 1
   else 
-    return hash[key] = 1
+    hash[key] = 1
   end
 end
